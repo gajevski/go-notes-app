6c5963e28bd8817c2231d96104a128b7302f216f
gajevski <mikolajxgajewski@gmail.com>
2025-01-09 20:03:04 +0100

    create - append to a file

--- cmd/create.go
+++ cmd/create.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -12,9 +13,22 @@ var createCmd = &cobra.Command{
 	Short:   "create short",
 	Long:    "create long",
 	Args:    cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("args: %s\n", args[0])
-	},
+	Run:     create,
+}
+
+func create(cmd *cobra.Command, args []string) {
+	file, err := os.OpenFile("test.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+	len, err := file.WriteString(args[0] + "\n")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("File name %s\n", file.Name())
+	fmt.Printf("File length %d\n", len)
 }
 
 func init() {
